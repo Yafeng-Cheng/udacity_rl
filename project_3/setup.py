d19fd5c5281719df31d5bf440e1b2abc27882a31
Yafeng Cheng <yafeng.cheng@kpmg.co.uk>
2019-04-04 22:19:08 +0100

    project_3skeleton

---
+++ project_3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Train a double-jointed ararm to touch a moving object',
+    author='Yafeng Cheng',
+    license='',
+)
